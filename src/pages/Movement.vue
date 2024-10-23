d53413aa334167b5be0a4a0d3724ed04383a7119
Carlos Davi <carlos@datasource.expert>
2024-10-23 20:18:02 -0300

    Feat: Ajustado botões de ações em movimentações

--- src/pages/Movement.vue
+++ src/pages/Movement.vue
@@ -7,6 +7,22 @@ defineOptions({
 </script>
 <template>
   <section>
-    <TitlePage title="Gerenciamento de movimentações"/>
+    <header class="row justify-between no-wrap bg-grey-3">
+      <div class="col-8">
+        <TitlePage title="Gerenciamento de movimentações"/>
+      </div>
+      <div class="col-4 row items-center justify-end q-gutter-x-sm">
+        <q-btn flat color="black" icon-right="settings" label="Categorias" unelevated no-caps/>
+        <q-btn color="negative" icon-right="remove" label="Saida" unelevated no-caps/>
+        <q-btn
+          color="positive"
+          icon-right="add"
+          label="Entrada"
+          class="q-mr-sm"
+          unelevated
+          no-caps
+        />
+      </div>
+    </header>
   </section>
 </template>
