ac92247073926ed4cf99a1541c30b2062e4c3f26
Carlos Davi <carlos@datasource.expert>
2024-10-25 00:08:26 -0300

    Feat: Adicionado banner na pagina de feed

---
+++ src/components/shared/Banner.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'Banner',
+});
+
+const slideBanner = ref<number>(1);
+</script>
+<template>
+  <div class="full-width bg-blue-grey-1">
+    <q-responsive
+      style="width: 100%; height: 300px; max-height: 300px; overflow: hidden"
+    >
+      <q-carousel
+        animated
+        v-model="slideBanner"
+        arrows
+        infinite
+        :autoplay="4000"
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        class="q-pa-none "
+      >
+        <q-carousel-slide :name="1" class="q-pa-none">
+          <q-img
+            src="/images/banner/frame-1.png"
+            fit="contain"
+            class="img-banner "
+          />
+        </q-carousel-slide>
+        <q-carousel-slide :name="2" class="q-pa-none">
+          <q-img
+            src="/images/banner/frame-2.png"
+            fit="contain"
+            class="img-banner "
+          />
+        </q-carousel-slide>
+      </q-carousel>
+    </q-responsive>
+  </div>
+</template>
--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import TitlePage from 'src/components/shared/TitlePage.vue';
+import Banner from 'src/components/shared/Banner.vue';
 
 defineOptions({
   name: 'Feed',
@@ -7,6 +8,13 @@ defineOptions({
 </script>
 <template>
   <section>
-    <TitlePage title="Painel de notícias"/>
+    <header class="row justify-between no-wrap bg-grey-3">
+      <div class="col-5">
+        <TitlePage title="Painel de notícias"/>
+      </div>
+    </header>
+    <main>
+      <Banner />
+    </main>
   </section>
 </template>
