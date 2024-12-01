bd2fa537a22ee97f8507a5b49ea2f102a2a286a2
Carlos Davi <carlosdavi004@gmail.com>
2024-11-30 23:26:28 -0300

    Build: Mudança nas configurações do quasar

--- quasar.config.js
+++ quasar.config.js
@@ -14,8 +14,8 @@ module.exports = configure((/* ctx */) => ({
     // 'themify',
     // 'line-awesome',
     // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
-    'roboto-font', // optional, you are not bound to it
-    'material-icons', // optional, you are not bound to it
+    'roboto-font',
+    'material-icons',
   ],
   build: {
     env: process.env,
@@ -23,7 +23,7 @@ module.exports = configure((/* ctx */) => ({
       browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
       node: 'node20',
     },
-    vueRouterMode: 'history', // available values: 'hash', 'history'
+    vueRouterMode: 'history',
     vitePlugins: [
       [
         'vite-plugin-checker',
@@ -40,7 +40,7 @@ module.exports = configure((/* ctx */) => ({
     ],
   },
   devServer: {
-    open: true, // opens browser window automatically
+    open: true,
   },
   framework: {
     config: {},
@@ -50,13 +50,11 @@ module.exports = configure((/* ctx */) => ({
   animations: 'all',
   ssr: {
     pwa: false,
-    prodPort: 3000, // The default port that the production server should use
-    middlewares: [
-      'render', // keep this as last one
-    ],
+    prodPort: 3000,
+    middlewares: ['render'],
   },
   pwa: {
-    workboxMode: 'generateSW', // or 'injectManifest'
+    workboxMode: 'generateSW',
     injectPwaMetaTags: true,
     swFilename: 'sw.js',
     manifestFilename: 'manifest.json',
@@ -68,7 +66,7 @@ module.exports = configure((/* ctx */) => ({
   },
   electron: {
     inspectPort: 5858,
-    bundler: 'packager', // 'packager' or 'builder'
+    bundler: 'packager',
     packager: {},
     builder: {
       appId: 'themplus-frontend',
