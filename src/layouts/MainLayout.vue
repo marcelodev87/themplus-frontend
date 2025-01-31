b69cc4d0d23e5dc46b1cbd2905be2c3b1fa00b31
Carlos Davi <carlosdavi004@gmail.com>
2025-01-30 23:57:13 -0300

    Style: Alteração de visual do menu lateral

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@ import FormEnterprise from 'src/components/forms/FormEnterprise.vue';
 import Navbar from 'src/components/headers/Navbar.vue';
 import EmailInfo from 'src/components/info/EmailInfo.vue';
 import FormViewEnterprise from 'src/components/forms/FormViewEnterprise.vue';
-import { onMounted, ref, watch } from 'vue';
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useAuthStore } from 'src/stores/auth-store';
 import { storeToRefs } from 'pinia';
@@ -24,15 +24,6 @@ const showFormPerfil = ref<boolean>(false);
 const showFormEnterprise = ref<boolean>(false);
 const showEmailInfo = ref<boolean>(false);
 const showFormViewEnterprise = ref<boolean>(false);
-
-const menuList = ref<
-  {
-    icon: string;
-    label: string;
-    separator: boolean;
-    name: string;
-  }[]
->([]);
 
 const isActive = (routeName: string) => {
   return (
@@ -64,119 +55,6 @@ const closeViewEnterprise = (): void => {
 const closeFormEnterprise = (): void => {
   showFormEnterprise.value = false;
 };
-const mountRoute = () => {
-  menuList.value = [];
-  menuList.value.push({
-    icon: 'rss_feed',
-    label: 'Feed',
-    separator: true,
-    name: 'admin-feed',
-  });
-
-  // {
-  //   icon: 'warning',
-  //   label: 'Alertas',
-  //   separator: true,
-  //   name: 'admin-alert',
-  // },
-
-  if (enterprisePosition.value === 'client') {
-    menuList.value.push({
-      icon: 'space_dashboard',
-      label: 'Dashboard',
-      separator: true,
-      name: 'admin-dashboard',
-    });
-    menuList.value.push({
-      icon: 'attach_money',
-      label: 'Movimentações',
-      separator: true,
-      name: 'admin-movement',
-    });
-    menuList.value.push({
-      icon: 'calendar_month',
-      label: 'Agendamentos',
-      separator: true,
-      name: 'admin-scheduling',
-    });
-    menuList.value.push({
-      icon: 'savings',
-      label: 'Contabilidade',
-      separator: true,
-      name: 'admin-financial-control',
-    });
-    menuList.value.push({
-      icon: 'category',
-      label: 'Categorias',
-      separator: true,
-      name: 'admin-category',
-    });
-    menuList.value.push({
-      icon: 'account_balance',
-      label: 'Contas',
-      separator: true,
-      name: 'admin-account',
-    });
-    menuList.value.push({
-      icon: 'content_paste_search',
-      label: 'Registros',
-      separator: true,
-      name: 'admin-records',
-    });
-  }
-  if (enterprisePosition.value === 'counter') {
-    menuList.value.push({
-      icon: 'person_search',
-      label: 'Solicitações',
-      separator: true,
-      name: 'admin-order',
-    });
-    menuList.value.push({
-      icon: 'how_to_reg',
-      label: 'Vínculos',
-      separator: true,
-      name: 'admin-bond',
-    });
-    menuList.value.push({
-      icon: 'work',
-      label: 'Organização',
-      separator: true,
-      name: 'admin-enterprise',
-    });
-  }
-  if (user.value?.position === 'admin') {
-    menuList.value.push({
-      icon: 'group_work',
-      label: 'Departamentos',
-      separator: true,
-      name: 'admin-departments',
-    });
-    menuList.value.push({
-      icon: 'person',
-      label: 'Usuários',
-      separator: true,
-      name: 'admin-users',
-    });
-  }
-  if (
-    enterpriseCreated.value === null &&
-    enterprisePosition.value === 'client' &&
-    user.value?.enterprise_id === user.value?.view_enterprise_id
-  ) {
-    menuList.value.push({
-      icon: 'holiday_village',
-      label: 'Filiais',
-      separator: true,
-      name: 'admin-office',
-    });
-  }
-  menuList.value.push({
-    icon: 'help',
-    label: 'Ajuda',
-    separator: true,
-    name: 'admin-help',
-  });
-};
 const changeShowMenuList = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 };
@@ -185,14 +63,6 @@ const closeDrawer = (): void => {
     leftDrawerOpen.value = false;
   }
 };
-
-watch(user, () => {
-  mountRoute();
-});
-
-onMounted(() => {
-  mountRoute();
-});
 </script>
 <template>
   <q-layout view="hHh lpR fFf">
@@ -209,7 +79,7 @@ onMounted(() => {
     <q-drawer
       v-model="leftDrawerOpen"
       :show-if-above="!$q.screen.lt.md"
-      :width="200"
+      :width="220"
       :breakpoint="700"
       side="left"
       behavior="desktop"
@@ -219,20 +89,209 @@ onMounted(() => {
       <q-scroll-area class="fit">
         <q-list>
           <q-item
-            v-for="(menuItem, index) in menuList"
-            :key="index"
             clickable
-            :to="{ name: menuItem.name }"
-            :active="isActive(menuItem.name)"
+            :to="{ name: 'admin-feed' }"
+            :active="isActive('feed')"
             active-class=" active-option-menu text-bold"
             @click="closeDrawer"
           >
             <q-item-section avatar>
-              <q-icon :name="menuItem.icon" />
+              <q-icon name="rss_feed" />
             </q-item-section>
-            <q-item-section>
-              {{ menuItem.label }}
+            <q-item-section> Feed </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'client'"
+            clickable
+            :to="{ name: 'admin-dashboard' }"
+            :active="isActive('admin-dashboard')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="space_dashboard" />
             </q-item-section>
+            <q-item-section> Dashboard </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'client'"
+            clickable
+            :to="{ name: 'admin-movement' }"
+            :active="isActive('admin-movement')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="attach_money" />
+            </q-item-section>
+            <q-item-section> Movimentações </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'client'"
+            clickable
+            :to="{ name: 'admin-scheduling' }"
+            :active="isActive('admin-scheduling')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="calendar_month" />
+            </q-item-section>
+            <q-item-section> Agendamentos </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'client'"
+            clickable
+            :to="{ name: 'admin-financial-control' }"
+            :active="isActive('admin-financial-control')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="savings" />
+            </q-item-section>
+            <q-item-section> Contabilidade </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'client'"
+            clickable
+            :to="{ name: 'admin-records' }"
+            :active="isActive('admin-records')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="content_paste_search" />
+            </q-item-section>
+            <q-item-section> Atividades </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'counter'"
+            clickable
+            :to="{ name: 'admin-order' }"
+            :active="isActive('admin-order')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="person_search" />
+            </q-item-section>
+            <q-item-section> Solicitações </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'counter'"
+            clickable
+            :to="{ name: 'admin-bond' }"
+            :active="isActive('admin-bond')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="how_to_reg" />
+            </q-item-section>
+            <q-item-section> Vínculos </q-item-section>
+          </q-item>
+          <q-item
+            v-show="enterprisePosition === 'counter'"
+            clickable
+            :to="{ name: 'admin-enterprise' }"
+            :active="isActive('admin-enterprise')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="work" />
+            </q-item-section>
+            <q-item-section> Organização </q-item-section>
+          </q-item>
+          <q-expansion-item
+            expand-separator
+            icon="settings"
+            label="Configurações"
+            dense-toggle
+          >
+            <q-item
+              v-show="enterprisePosition === 'client'"
+              clickable
+              :to="{ name: 'admin-category' }"
+              :active="isActive('admin-category')"
+              active-class=" active-option-menu text-bold"
+              @click="closeDrawer"
+            >
+              <q-item-section avatar>
+                <q-icon name="category" />
+              </q-item-section>
+              <q-item-section> Categorias </q-item-section>
+            </q-item>
+            <q-item
+              v-show="enterprisePosition === 'client'"
+              clickable
+              :to="{ name: 'admin-account' }"
+              :active="isActive('admin-account')"
+              active-class=" active-option-menu text-bold"
+              @click="closeDrawer"
+            >
+              <q-item-section avatar>
+                <q-icon name="account_balance" />
+              </q-item-section>
+              <q-item-section> Contas </q-item-section>
+            </q-item>
+            <q-item
+              v-show="user?.position === 'admin'"
+              clickable
+              :to="{ name: 'admin-departments' }"
+              :active="isActive('admin-departments')"
+              active-class=" active-option-menu text-bold"
+              @click="closeDrawer"
+            >
+              <q-item-section avatar>
+                <q-icon name="group_work" />
+              </q-item-section>
+              <q-item-section> Departamentos </q-item-section>
+            </q-item>
+            <q-item
+              v-show="
+                enterpriseCreated === null &&
+                enterprisePosition === 'client' &&
+                user?.enterprise_id === user?.view_enterprise_id
+              "
+              clickable
+              :to="{ name: 'admin-office' }"
+              :active="isActive('admin-office')"
+              active-class=" active-option-menu text-bold"
+              @click="closeDrawer"
+            >
+              <q-item-section avatar>
+                <q-icon name="holiday_village" />
+              </q-item-section>
+              <q-item-section> Filiais </q-item-section>
+            </q-item>
+            <q-item
+              v-show="user?.position === 'admin'"
+              clickable
+              :to="{ name: 'admin-users' }"
+              :active="isActive('admin-users')"
+              active-class=" active-option-menu text-bold"
+              @click="closeDrawer"
+            >
+              <q-item-section avatar>
+                <q-icon name="person" />
+              </q-item-section>
+              <q-item-section> Usuários </q-item-section>
+            </q-item>
+          </q-expansion-item>
+
+          <q-item
+            clickable
+            :to="{ name: 'admin-help' }"
+            :active="isActive('admin-help')"
+            active-class=" active-option-menu text-bold"
+            @click="closeDrawer"
+          >
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+            <q-item-section> Ajuda </q-item-section>
           </q-item>
         </q-list>
       </q-scroll-area>
